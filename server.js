041534780e6dea01c65600c5bac3381f4dc77804
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-01-27 09:46:43 -0800

    basic example of middleware

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public')); // middleware that will make routing easy
                                                 // __dirname is the pathname of the project
 
+app.use((req, res, next) => {
+   var now = new Date().toString();
+
+   console.log(`${now}: ${req.method} ${req.url}`);
+   next();
+});
+
 hbs.registerHelper('getCurrentYear', () => {
    return new Date().getFullYear();
 });
