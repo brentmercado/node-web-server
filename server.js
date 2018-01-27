ba36b33fff8aaa15704fb1c3d10455a984480c06
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-01-27 08:44:19 -0800

    Footer partial

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const hbs = require('hbs');   // handlbars is a view for express (helpful)
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public')); // middleware that will make routing easy
                                                 // __dirname is the pathname of the project
